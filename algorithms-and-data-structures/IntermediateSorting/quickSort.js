7a634c3f27b2f3ba98b72b36934de92054dd6edf
GabsFranke <frankegabriel@gmail.com>
2022-09-27 17:18:52 -0300

    Quick Sort

---
+++ algorithms-and-data-structures/IntermediateSorting/quickSort.js
@@ -0,0 +1,35 @@
+// Helper function
+function pivot(arr, start = 0, end = arr.length - 1) {
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
+
+  let pivot = arr[start];
+  let swapIdx = start;
+
+  for (let i = start + 1; i <= end; i++) {
+    if (pivot > arr[i]) {
+      swapIdx++;
+      swap(arr, swapIdx, i);
+    }
+  }
+
+  swap(arr, start, swapIdx);
+
+  return swapIdx;
+}
+
+// Time complexity: Average: O(n log n), Worst case: O(n^2)
+// Space complexity: O(log n)
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    let pivotIndex = pivot(arr, left, right);
+    //left
+    quickSort(arr, left, pivotIndex - 1);
+    //right
+    quickSort(arr, pivotIndex + 1, right);
+  }
+  return arr;
+}
+
+console.log(quickSort([100, -3, 2, 4, 6, 9, 1, 2, 5, 3, 23]));
